235faaef129f58f6d94c75c6bb1b949645b65bda
ssrz1 <sajjadraza00@gmail.com>
2018-09-18 11:53:14 +0500

    Please enter the commit message for your changes. Lines starting

---
+++ project1/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hey, YOU! MR.Syed Sajjad Raza Zaidi</Text>
+        <Text>blah blah blah</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
