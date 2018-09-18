c082e05f24c09a399b203e0de329787d25fe3a5f
ssrz1 <sajjadraza00@gmail.com>
2018-09-18 21:49:47 +0500

    flex-box

--- App.js
+++ App.js
@@ -4,13 +4,11 @@ import { StyleSheet, Text, View, StatusBar } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <StatusBar hidden />
         <Text style={styles.defaultText}>Hey,</Text>
         <Text style={styles.defaultText}>you</Text>
         <Text style={styles.defaultText}>MR</Text>
-        //we can use multiple metods of style class by using Array.
-        //and since we are not restricted to adding one method of style class
         <Text style={[styles.defaultText, styles.selectedText]}>Sajjad</Text>
       </View>
     )
@@ -24,6 +22,11 @@ const styles = StyleSheet.create({
     margin: 5,
     borderWidth: StyleSheet.hairlineWidth,
     color: 'black'
+  },
+  container: {
+    flexDirection: 'row',
+    backgroundColor: '#DDD',
+    justifyContent: 'center'
   },
   //s
   selectedText: {
